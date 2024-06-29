0988307dc037af2e0c8965e454a72e37813967ba
MindstormingAB <s.elander@mindstorming.se>
2024-06-29 19:16:55 +0200

    Added Vision to Sanity Studio

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
+import {visionTool} from "@sanity/vision";
 import schemas from "./sanity/schemas";
 
 const config = defineConfig({
@@ -8,7 +9,7 @@ const config = defineConfig({
     title: "Sandrine's portfolio",
     apiVersion: "2024-05-12",
     basePath: "/admin",
-    plugins: [structureTool()],
+    plugins: [structureTool(), visionTool()],
     schema: { types: schemas }
 });
 
